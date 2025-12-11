39b4b055e17fa101f451dd76b637219f1187de1c
bessaliy <bessy8295@gmail.com>
2025-12-11 22:59:26 +0700

    2 Исправлены ошибки, начала делать моки

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,4 +1,4 @@
-import EditFormView from '../views/form-edit-view.js';
+import EditFormView from '../views/edit-form-view.js';
 import NewFormView from '../views/new-form-view.js';
 import EventsListView from '../views/events-list-view.js';
 import PointView from '../views/point-view.js';
@@ -11,16 +11,16 @@ export default class BoardPresenter {
   sortComponent = new SortView();
   listComponent = new EventsListView();
 
-  constructor({ headerContainer, container }) {
+  constructor({ headerContainer, mainContainer }) {
     this.headerContainer = headerContainer;
-    this.container = container;
+    this.mainContainer = mainContainer;
   }
 
   init() {
     render(new NewFormView(), this.listComponent.getElement());
     render(new FiltersView(), this.headerContainer);
-    render(this.sortComponent, this.container);
-    render(this.listComponent, this.container);
+    render(this.sortComponent, this.mainContainer);
+    render(this.listComponent, this.mainContainer);
 
     for (let i = 0; i < 3; i++) {
       render(new PointView(), this.listComponent.getElement());
---
+++ src/templates.js
@@ -0,0 +1,38 @@
+const pointObjectTemplate = {
+  'id': 'f4b62099-293f-4c3d-a702-94eec4a2808c',
+  'base_price': 1100,
+  'date_from': '2019-07-10T22:55:56.845Z',
+  'date_to': '2019-07-11T11:22:13.375Z',
+  'destination': 'bfa5cb75-a1fe-4b77-a83c-0e528e910e04',
+  'is_favorite': false,
+  'offers': [
+    'b4c3e4e6-9053-42ce-b747-e281314baa31'
+  ],
+  'type': 'taxi'
+};
+const pointsTemplate = [];
+
+const destinationTemplate = {
+  'id': 'cfe416cq-10xa-ye10-8077-2fs9a01edcab',
+  'description': 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+  'name': 'Chamonix',
+  'pictures': [
+    {
+      'src': 'http://picsum.photos/300/200?r=0.0762563005163317',
+      'description': 'Chamonix parliament building'
+    }
+  ]
+};
+const destinationsTemplate = [];
+
+const offerTemplate = {
+  'type': 'taxi',
+  'offers': [
+    {
+      'id': 'b4c3e4e6-9053-42ce-b747-e281314baa31',
+      'title': 'Upgrade to a business class',
+      'price': 120
+    }
+  ]
+};
+const offersTemplate = [];
