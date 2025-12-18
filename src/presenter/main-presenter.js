7958f4489a5dd680b7e9226067156b6a8f0b2a41
bessaliy <bessy8295@gmail.com>
2025-12-18 18:08:20 +0700

    4. Исправляет ошибки 2 раздела

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -3,37 +3,36 @@ import NewFormView from '../views/new-form-view.js';
 import EventsListView from '../views/events-list-view.js';
 import PointView from '../views/point-view.js';
 import SortView from '../views/sort-view.js';
-import FiltersView from '../views/filters-view';
+import FiltersView from '../views/filters-view.js';
 
 import { render } from '../render.js';
-
 
 export default class MainPresenter {
   sortComponent = new SortView();
   listComponent = new EventsListView();
 
-  constructor({ headerContainer, mainContainer, pointModel }) {
-    this.headerContainer = headerContainer;
-    this.mainContainer = mainContainer;
+  constructor({ filterElement, tripElement, pointModel }) {
+    this.filterElement = filterElement;
+    this.tripElement = tripElement;
     this.pointModel = pointModel;
   }
 
   init() {
     this.points = [...this.pointModel.getPoints()];
-    render(new FiltersView(), this.headerContainer);
-    render(this.sortComponent, this.mainContainer);
-    render(this.listComponent, this.mainContainer);
+    render(new FiltersView(), this.filterElement);
+    render(this.sortComponent, this.tripElement);
+    render(this.listComponent, this.tripElement);
     render(new NewFormView({
       point: this.points[0],
-      destination: this.pointModel.getDestinationsById(this.points[0].destination),
+      destination: this.pointModel.getDestinationById(this.points[0].destination),
       offers: this.pointModel.getOffersByType(this.points[0].type)
     }), this.listComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 1; i < this.points.length; i++) {
 
       render(new PointView({
         point: this.points[i],
-        destination: this.pointModel.getDestinationsById(this.points[i].destination),
+        destination: this.pointModel.getDestinationById(this.points[i].destination),
         offers: this.pointModel.getOffersByType(this.points[i].type)
       }), this.listComponent.getElement());
 
